3f454516df3e2a7ffa7303771334e0f685a275cb
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-10-12 23:21:53 +0200

    Working on logs

--- Runtime/Feedbacks/Time/WaitTimeFeedback.cs
+++ Runtime/Feedbacks/Time/WaitTimeFeedback.cs
@@ -11,6 +11,16 @@ namespace Juce.Feedbacks
         [Header("Values")]
         [SerializeField] private float time = default;
 
+        public override string GetFeedbackInfo()
+        {
+            if (time <= 0.0f)
+            {
+                return string.Empty;
+            }
+
+            return $"Time: { time }";
+        }
+
         public override void OnExectue(FlowContext context, SequenceTween sequenceTween)
         {
             sequenceTween.AppendWaitTime(time);
