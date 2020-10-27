8f94c28d84106015a8f44756c2679a6ac9b6eed2
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-10-27 22:19:00 +0100

    Text mesh pro feedbacks

---
+++ Runtime/Feedbacks/TextMeshPro/TextMeshProParagraphSpacingFeedback.cs
@@ -0,0 +1,96 @@
+﻿
+#if JUCE_TEXT_MESH_PRO_EXTENSIONS
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Juce.Tween;
+using TMPro;
+
+namespace Juce.Feedbacks
+{
+    [FeedbackIdentifier("Pharagraph Spacing", "TextMeshPro/")]
+    public class TextMeshProParagraphSpacingFeedback : Feedback
+    {
+        [Header(FeedbackSectionsUtils.TargetSection)]
+        [SerializeField] private TextMeshProUGUI target = default;
+
+        [Header(FeedbackSectionsUtils.ValuesSection)]
+        [SerializeField] private StartEndFloatProperty value = default;
+
+        [Header(FeedbackSectionsUtils.TimingSection)]
+        [SerializeField] [Min(0)] private float delay = default;
+        [SerializeField] [Min(0)] private float duration = 1.0f;
+
+        [Header(FeedbackSectionsUtils.EasingSection)]
+        [SerializeField] private EasingProperty easing = default;
+
+        [Header(FeedbackSectionsUtils.LoopSection)]
+        [SerializeField] private LoopProperty looping = default;
+
+        public TextMeshProUGUI Target { get => target; set => target = value; }
+        public StartEndFloatProperty Value => value;
+        public float Delay { get => delay; set => delay = Mathf.Max(0, value); }
+        public float Duration { get => duration; set => duration = Mathf.Max(0, value); }
+        public EasingProperty Easing => easing;
+        public LoopProperty Looping => looping;
+
+        public override bool GetFeedbackErrors(out string errors)
+        {
+            if (target == null)
+            {
+                errors = ErrorUtils.TargetNullErrorMessage;
+                return true;
+            }
+
+            errors = string.Empty;
+            return false;
+        }
+
+        public override string GetFeedbackTargetInfo()
+        {
+            return target != null ? target.gameObject.name : string.Empty;
+        }
+
+        public override void GetFeedbackInfo(ref List<string> infoList)
+        {
+            InfoUtils.GetTimingInfo(ref infoList, delay, duration);
+            InfoUtils.GetStartEndFloatPropertyInfo(ref infoList, value);
+        }
+
+        public override ExecuteResult OnExecute(FlowContext context, SequenceTween sequenceTween)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            Tween.Tween delayTween = null;
+
+            if (delay > 0)
+            {
+                delayTween = new WaitTimeTween(delay);
+                sequenceTween.Append(delayTween);
+            }
+
+            if (value.UseStartValue)
+            {
+                sequenceTween.Append(target.TweenParagraphSpacingSpacing(value.StartValue, 0.0f));
+            }
+
+            Tween.Tween progressTween = target.TweenParagraphSpacingSpacing(value.EndValue, duration);
+            sequenceTween.Append(progressTween);
+
+            EasingUtils.SetEasing(sequenceTween, easing);
+            LoopUtils.SetLoop(sequenceTween, looping);
+
+            ExecuteResult result = new ExecuteResult();
+            result.DelayTween = delayTween;
+            result.ProgresTween = progressTween;
+
+            return result;
+        }
+    }
+}
+
+#endif
