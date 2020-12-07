14983fdf0f0a5872e3f9c8f84fb93741179869d3
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-12-07 23:59:16 +0100

    Working on anchors

---
+++ Runtime/Feedbacks/RectTransform/RectTransformAnchorMaxFeedback.cs
@@ -0,0 +1,115 @@
+﻿using Juce.Tween;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juce.Feedbacks
+{
+    [FeedbackIdentifier("Anchor Max", "RectTransform/")]
+    public class RectTransformAnchorMaxFeedback : Feedback
+    {
+        [Header(FeedbackSectionsUtils.TargetSection)]
+        [SerializeField] private RectTransform target = default;
+
+        [Header(FeedbackSectionsUtils.ValuesSection)]
+        [SerializeField] private StartEndVector2Property value = default;
+
+        [Header(FeedbackSectionsUtils.TimingSection)]
+        [SerializeField] [Min(0)] private float delay = default;
+
+        [SerializeField] [Min(0)] private float duration = 1.0f;
+
+        [Header(FeedbackSectionsUtils.EasingSection)]
+        [SerializeField] private EasingProperty easing = default;
+
+        [Header(FeedbackSectionsUtils.LoopSection)]
+        [SerializeField] private LoopProperty looping = default;
+
+        public RectTransform Target { get => target; set => target = value; }
+        public StartEndVector2Property Value => value;
+        public float Delay { get => delay; set => delay = Mathf.Max(0, value); }
+        public float Duration { get => duration; set => duration = Mathf.Max(0, value); }
+        public EasingProperty Easing => easing;
+        public LoopProperty Looping => looping;
+
+        public override bool GetFeedbackErrors(out string errors)
+        {
+            if (target == null)
+            {
+                errors = ErrorUtils.TargetNullErrorMessage;
+                return true;
+            }
+
+            errors = string.Empty;
+            return false;
+        }
+
+        public override string GetFeedbackTargetInfo()
+        {
+            return target != null ? target.gameObject.name : string.Empty;
+        }
+
+        public override void GetFeedbackInfo(ref List<string> infoList)
+        {
+            InfoUtils.GetTimingInfo(ref infoList, delay, duration);
+            InfoUtils.GetStartEndVector2PropertyInfo(ref infoList, value);
+        }
+
+        public override ExecuteResult OnExecute(FlowContext context, SequenceTween sequenceTween)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            Tween.Tween delayTween = null;
+
+            if (delay > 0)
+            {
+                delayTween = new WaitTimeTween(delay);
+                sequenceTween.Append(delayTween);
+            }
+
+            if (value.UseStartValue)
+            {
+                SequenceTween startSequence = new SequenceTween();
+
+                if (value.UseStartX)
+                {
+                    startSequence.Join(target.TweenAnchorMaxX(value.StartValueX, 0.0f));
+                }
+
+                if (value.UseStartY)
+                {
+                    startSequence.Join(target.TweenAnchorMaxY(value.StartValueY, 0.0f));
+                }
+
+                sequenceTween.Append(startSequence);
+            }
+
+            SequenceTween endSequence = new SequenceTween();
+
+            if (value.UseEndX)
+            {
+                endSequence.Join(target.TweenAnchorMaxX(value.EndValueX, duration));
+            }
+
+            if (value.UseEndY)
+            {
+                endSequence.Join(target.TweenAnchorMaxY(value.EndValueY, duration));
+            }
+
+            Tween.Tween progressTween = endSequence;
+
+            sequenceTween.Append(endSequence);
+
+            EasingUtils.SetEasing(sequenceTween, easing);
+            LoopUtils.SetLoop(sequenceTween, looping);
+
+            ExecuteResult result = new ExecuteResult();
+            result.DelayTween = delayTween;
+            result.ProgresTween = progressTween;
+
+            return result;
+        }
+    }
+}
