b85b54c00ddbd0953ed78198df2f9cf545f601a2
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2020-12-21 19:00:29 +0100

    Added kill and reset feedback

---
+++ Runtime/Feedbacks/FeedbacksPlayer/FeedbacksPlayerKillAndResetFeedback.cs
@@ -0,0 +1,78 @@
+﻿using Juce.Tween;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juce.Feedbacks
+{
+    [FeedbackIdentifier("Kill and reset", "Feedbacks Player/")]
+    public class FeedbacksPlayerKillAndResetFeedback : Feedback
+    {
+        [Header(FeedbackSectionsUtils.TargetSection)]
+        [SerializeField] private FeedbacksPlayer target = default;
+
+        [Header(FeedbackSectionsUtils.TimingSection)]
+        [SerializeField] [Min(0)] private float delay = default;
+
+        public FeedbacksPlayer Target { get => target; set => target = value; }
+        public float Delay { get => delay; set => delay = Mathf.Max(0, value); }
+
+        public override bool GetFeedbackErrors(out string errors)
+        {
+            if (target == null)
+            {
+                errors = ErrorUtils.TargetNullErrorMessage;
+                return true;
+            }
+
+            errors = string.Empty;
+            return false;
+        }
+
+        public override string GetFeedbackTargetInfo()
+        {
+            return target != null ? target.name : string.Empty;
+        }
+
+        public override void GetFeedbackInfo(ref List<string> infoList)
+        {
+            InfoUtils.GetTimingInfo(ref infoList, delay);
+        }
+
+        public override void OnReset()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            target.KillAndReset();
+        }
+
+        public override ExecuteResult OnExecute(FlowContext context, SequenceTween sequenceTween)
+        {
+            if (target == null)
+            {
+                return null;
+            }
+
+            Tween.Tween delayTween = null;
+
+            if (delay > 0)
+            {
+                delayTween = new WaitTimeTween(delay);
+                sequenceTween.Append(delayTween);
+            }
+
+            ExecuteResult result = new ExecuteResult();
+
+            sequenceTween.AppendCallback(() =>
+            {
+                target.KillAndReset();
+            });
+
+            result.DelayTween = delayTween;
+
+            return result;
+        }
+    }
+}
