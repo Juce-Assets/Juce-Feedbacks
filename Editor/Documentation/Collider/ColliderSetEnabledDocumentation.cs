eb6f1e9e552f4745ca731479761ae632784ea225
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2021-02-20 20:06:13 +0100

    Collider enabled

---
+++ Editor/Documentation/Collider/ColliderSetEnabledDocumentation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Juce.Feedbacks
+{
+    internal class ColliderSetEnabledDocumentation : IFeedbackDocumentation
+    {
+        public Type FeedbackType => typeof(ColliderSetEnabledFeedback);
+
+        public void DrawDocumentation()
+        {
+            GUILayout.Label("Enables or disables the target Collider", EditorStyles.wordWrappedLabel);
+
+            EditorGUILayout.Space(2);
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GUILayout.Label("- Target: Collider that is going to be enabled/disabled", EditorStyles.wordWrappedLabel);
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GUILayout.Label("- Set Enabled: enables or disables the Collider", EditorStyles.wordWrappedLabel);
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GenericsDocumentation.DelayDocumentation();
+            }
+        }
+    }
+}
---
+++ Editor/Documentation/Collider2D/Collider2DSetEnabledDocumentation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Juce.Feedbacks
+{
+    internal class Collider2DSetEnabledDocumentation : IFeedbackDocumentation
+    {
+        public Type FeedbackType => typeof(Collider2DSetEnabledFeedback);
+
+        public void DrawDocumentation()
+        {
+            GUILayout.Label("Enables or disables the target Collider2D", EditorStyles.wordWrappedLabel);
+
+            EditorGUILayout.Space(2);
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GUILayout.Label("- Target: Collider2D that is going to be enabled/disabled", EditorStyles.wordWrappedLabel);
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GUILayout.Label("- Set Enabled: enables or disables the Collider2D", EditorStyles.wordWrappedLabel);
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                GenericsDocumentation.DelayDocumentation();
+            }
+        }
+    }
+}
